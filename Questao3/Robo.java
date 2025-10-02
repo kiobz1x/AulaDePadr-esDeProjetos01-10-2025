26b079429077cdcdb71dfb0cb2d39f4e69c28d72
Marianne-Remigio <mariannelimaremigio7@gmail.com>
2025-10-01 21:08:36 -0300

    Create Robo.java

---
+++ Questao3/Robo.java
@@ -0,0 +1,10 @@
+package org.example.exercicio3;
+
+
+public class Robo implements Funcionario {
+
+    @Override
+    public void trabalhar() {
+        System.out.println("Robô está trabalhando.");
+    }
+}
