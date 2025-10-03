6f57021a1637ab669e5593788125ced87adf21d8
Kaio <kaio.bezerra1809@gmail.com>
2025-10-03 10:11:44 -0300

    Create Pedido.java

---
+++ Questão1/Pedido.java
@@ -0,0 +1,37 @@
+package org.example.exercicio1;
+
+public class Pedido {
+
+    private int id;
+    private String descricao;
+    private double valor;
+
+    public Pedido(int id, String descricao, double valor) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
